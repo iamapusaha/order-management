c4c64c11832ca211614408f782ed28ecc95755cd
iamapusaha <apusaha392@gmail.com>
2024-05-23 22:28:53 +0600

    added product update api validation

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -5,7 +5,7 @@ import productValidationSchema from './product.joi.validation';
 const createProduct = async (req: Request, res: Response) => {
   try {
     const productData = req.body;
-    const { error, value } = productValidationSchema.validate(productData);
+    const { error } = productValidationSchema.validate(productData);
     if (error) {
       res.status(500).json({
         success: false,
@@ -58,26 +58,44 @@ const getSingleProduct = async (req: Request, res: Response) => {
 };
 
 const updateProductById = async (req: Request, res: Response) => {
+  const productData = req.body;
+
+  // Validate the product data
+  const { error } = productValidationSchema.validate(productData);
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      message: 'Validation error',
+      error: error.details,
+    });
+  }
   try {
     const { productId } = req.params;
     const result = await productService.updateSingleProductById(
       productId,
       req.body,
     );
-    if (!updateProductById) {
+
+    if (!result) {
       return res.status(404).json({
         success: false,
         message: 'Product not found',
         data: null,
       });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: 'Product updated successfully',
+        data: result,
+      });
     }
-    res.status(200).json({
-      success: true,
-      message: 'product update succesfully',
-      data: result,
-    });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: 'An error occurred while updating the product',
+      data: null,
+    });
   }
 };
 const deleteProductById = async (req: Request, res: Response) => {
